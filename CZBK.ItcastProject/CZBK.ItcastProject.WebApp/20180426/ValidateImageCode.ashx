0e8a737f9061b762a4ca49266e370c8fa8129dcf
supeng4587 <mailsupeng@sina.com>
2018-04-27 17:09:54 +0800

    20180427

---
+++ CZBK.ItcastProject/CZBK.ItcastProject.WebApp/20180426/ValidateImageCode.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="ValidateImageCode.ashx.cs" Class="CZBK.ItcastProject.WebApp._20180426.ValidateImageCode" %>
---
+++ CZBK.ItcastProject/CZBK.ItcastProject.WebApp/20180426/ValidateImageCode.ashx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CZBK.ItcastProject.Common;
+
+namespace CZBK.ItcastProject.WebApp._20180426
+{
+    /// <summary>
+    /// ValidateImageCode 的摘要说明
+    /// </summary>
+    public class ValidateImageCode : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            ValidateCode validateCode = new ValidateCode();
+            string code = validateCode.CreateValidateCode(4);
+            validateCode.CreateValidateGraphic(code, context);
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
